5358498fa76cbb31bfada0e2f8e6021a5416f545
tjrdl <tjrdlchlrh00@gmail.com>
2025-03-31 16:05:32 +0900

    form 데이터 전송
    httpServletRequest

--- spring_12_1/src/main/java/com/lgy/spring_12_1/HomeController.java
+++ spring_12_1/src/main/java/com/lgy/spring_12_1/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -16,24 +18,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
+	@RequestMapping("/board/confirmId")
+	// httpServletRequest 객체에 url 파라미터 값을 담는다
+	public String confirmId(HttpServletRequest httpServletRequest, Model model) {
+		// url 파라미터 id 값을 가져옴
+		String id = httpServletRequest.getParameter("id");
+		String pw = httpServletRequest.getParameter("pw");
+		
+		model.addAttribute("id",id);
+		model.addAttribute("pw",pw);
+		
+		return "board/confirmId";
+		
+	}
 }
