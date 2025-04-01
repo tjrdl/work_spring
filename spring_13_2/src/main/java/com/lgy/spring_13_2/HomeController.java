c636ba4a05f632fa4885dd2ca7b75807d4aff070
tjrdl <tjrdlchlrh00@gmail.com>
2025-04-01 10:33:58 +0900

    get post

--- spring_13_2/src/main/java/com/lgy/spring_13_2/HomeController.java
+++ spring_13_2/src/main/java/com/lgy/spring_13_2/HomeController.java
@@ -4,12 +4,15 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -33,7 +36,31 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return"index";
 	}
+
+	@RequestMapping("/index")
+	public String goIndex() {
+		return "index";
+	}
+
+
+	@RequestMapping(method = RequestMethod.GET, value = "/student")
+	public String goStudent(HttpServletRequest httpServletRequest, Model model) {
+		System.out.println("@# RequestMethod.get");
+		String id = httpServletRequest.getParameter("id");
+		model.addAttribute("studentId",id);
+		return "student";
+	}
+	@RequestMapping(method = RequestMethod.POST, value = "/student")
+	public ModelAndView goStudent(HttpServletRequest httpServletRequest) {
+		System.out.println("@# RequestMethod.POST");
+		String id = httpServletRequest.getParameter("id");
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("studentId",id);
+		mv.setViewName("student");
+		return mv;
+	}
+	
 	
 }
